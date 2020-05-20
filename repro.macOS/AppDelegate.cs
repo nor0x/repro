037452878eaa857403be6454560ea46bbe5270f4
Johnny <nor0x@users.noreply.github.com>
2020-05-20 12:45:38 +0200

    initial

---
+++ repro.macOS/AppDelegate.cs
@@ -0,0 +1,32 @@
+﻿using AppKit;
+using Foundation;
+using repro;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.MacOS;
+// also add a using for the Xamarin.Forms project, if the namespace is different to this file
+[Register("AppDelegate")]
+public class AppDelegate : FormsApplicationDelegate
+{
+    NSWindow window;
+    public AppDelegate()
+    {
+        var style = NSWindowStyle.Closable | NSWindowStyle.Resizable | NSWindowStyle.Titled;
+
+        var rect = new CoreGraphics.CGRect(200, 1000, 1024, 768);
+        window = new NSWindow(rect, style, NSBackingStore.Buffered, false);
+        window.Title = "Xamarin.Forms on Mac!"; // choose your own Title here
+        window.TitleVisibility = NSWindowTitleVisibility.Hidden;
+    }
+
+    public override NSWindow MainWindow
+    {
+        get { return window; }
+    }
+
+    public override void DidFinishLaunching(NSNotification notification)
+    {
+        Forms.Init();
+        LoadApplication(new App());
+        base.DidFinishLaunching(notification);
+    }
+}
